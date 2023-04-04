36cabb7d70531109b0c6153c04d8fb4b0265c381
joaquinC <joaquincibanal@gmail.com>
2023-04-04 19:13:38 -0300

    Reorder Folders and Assets and first steps with Enemies

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
     [SerializeField] private float diagonalSpeedMultiplier = 0.7f;
-    [SerializeField] private float checkDelay = 5f;
     [SerializeField] private float scale = 0.4f;
-    
-    
+
     private Rigidbody2D rb2d;
     private Animator animator;
     private Vector2 movement;
-    
-    private bool reposicionado;
-    private bool isCheckingPosition = false;
     private SpriteRenderer spriteRenderer;
 
     private void Start()
@@ -27,11 +22,8 @@ public class PlayerController : MonoBehaviour
     {
         rb2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        
         // Make player invisible
-        gameObject.SetActive(false);
-        this.reposicionado = false;
-        CheckPosition();
+       // gameObject.SetActive(false);
     }
     
     private void Update()
@@ -65,54 +57,5 @@ public class PlayerController : MonoBehaviour
        rb2d.MovePosition(rb2d.position + movement * (speed * Time.fixedDeltaTime));
     }
 
-    private void CheckPosition()
-    {
-        if (!this.reposicionado)
-        {
-            Debug.Log("Entre al check");
-            // Check if the player is on top of a collider
-            Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.2f);
-            bool isOnCollider = false;
-            foreach (Collider2D collider in colliders)
-            {
-                if (collider != GetComponent<Collider2D>())
-                {
-                    isOnCollider = true;
-                    break;
-                }
-            }
-
-            // If the player is not on a collider, activate the gameObject
-            if (!isOnCollider)
-            {
-                Debug.Log("Lo activo");
-                gameObject.SetActive(true);
-                reposicionado = true;
-            }
-            else
-            {
-                // Otherwise, reposition the player and check again after a delay
-                transform.position = new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), 0f);
-                Invoke("CheckPosition", checkDelay);
-            }
-
-            isCheckingPosition = false;
-        }
-    }
-
-    private void OnEnable()
-    {
-        if (!isCheckingPosition)
-        {
-            Debug.Log("Check position");
-            isCheckingPosition = true;
-            Invoke("CheckPosition", checkDelay);
-        }
-        else
-        {
-            Debug.Log("No Check");
-            gameObject.SetActive(true);
-        }
-    }
 }
 
---
+++ Assets/Scripts/PlayerPositionChecker.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PlayerPositionChecker : MonoBehaviour
+{
+    [SerializeField] private float checkDelay = 1.5f;
+    [SerializeField] private Vector2 checkAreaSize = new Vector2(1f, 1f);
+    
+    [SerializeField] private Transform parentTransform;
+    private bool isCheckingPosition = false;
+    private bool reposicionado;
+
+    private void Awake()
+    {
+        Debug.Log("Awake");
+        this.reposicionado = false;
+    }
+
+    private void Start()
+    {
+        // Obtén la referencia al transform del objeto padre
+        parentTransform = transform.parent;
+        CheckPosition();
+    }
+
+    private void CheckPosition()
+    {
+        if (!reposicionado)
+        {
+            Debug.Log("Entre al check");
+            // Check if the player is on top of a collider
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(parentTransform.position, 0.2f);
+            bool isOnCollider = false;
+            foreach (Collider2D collider in colliders)
+            {
+                Debug.Log("Parent transform es "+parentTransform+" y el componente collider es "+parentTransform.GetComponent<Collider2D>()+" y collider es "+collider);
+
+
+                if (collider != parentTransform.GetComponent<Collider2D>())
+                {
+                    isOnCollider = true;
+                    break;
+                }
+            }
+
+            // If the player is not on a collider, activate the gameObject
+            if (!isOnCollider)
+            {
+                Debug.Log("Lo activo");
+                parentTransform.gameObject.SetActive(true);
+                reposicionado = true;
+            }
+            else
+            {
+                // Otherwise, reposition the player and check again after a delay
+                parentTransform.position = new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), 0f);
+                Invoke("CheckPosition", checkDelay);
+            }
+
+            isCheckingPosition = false;
+        }
+    }
+
+
+}
